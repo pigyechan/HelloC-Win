e503e27f03b744d0c54ae1b5fcd7359eb55d2353
USER <USER@DESKTOP-LJP7SMB>
2024-01-03 15:17:54 +0900

    초기 커밋.

---
+++ HelloC#Win/Calculator.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace HelloC_Win
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HelloLabel = new System.Windows.Forms.Label();
+            this.SumNumbers = new System.Windows.Forms.Button();
+            this.Sum1 = new System.Windows.Forms.TextBox();
+            this.Sum2 = new System.Windows.Forms.TextBox();
+            this.SumResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // HelloLabel
+            // 
+            this.HelloLabel.AutoSize = true;
+            this.HelloLabel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.HelloLabel.Location = new System.Drawing.Point(151, 271);
+            this.HelloLabel.Name = "HelloLabel";
+            this.HelloLabel.Size = new System.Drawing.Size(132, 15);
+            this.HelloLabel.TabIndex = 0;
+            this.HelloLabel.Text = "여기를 클릭하세요";
+            this.HelloLabel.Click += new System.EventHandler(this.HelloLabel_Click);
+            // 
+            // SumNumbers
+            // 
+            this.SumNumbers.Location = new System.Drawing.Point(368, 121);
+            this.SumNumbers.Name = "SumNumbers";
+            this.SumNumbers.Size = new System.Drawing.Size(75, 23);
+            this.SumNumbers.TabIndex = 1;
+            this.SumNumbers.Text = "=";
+            this.SumNumbers.UseVisualStyleBackColor = true;
+            this.SumNumbers.Click += new System.EventHandler(this.SumNumbers_Click);
+            // 
+            // Sum1
+            // 
+            this.Sum1.Location = new System.Drawing.Point(70, 121);
+            this.Sum1.Name = "Sum1";
+            this.Sum1.Size = new System.Drawing.Size(100, 25);
+            this.Sum1.TabIndex = 2;
+            // 
+            // Sum2
+            // 
+            this.Sum2.Location = new System.Drawing.Point(214, 121);
+            this.Sum2.Name = "Sum2";
+            this.Sum2.Size = new System.Drawing.Size(100, 25);
+            this.Sum2.TabIndex = 3;
+            // 
+            // SumResult
+            // 
+            this.SumResult.Location = new System.Drawing.Point(483, 121);
+            this.SumResult.Name = "SumResult";
+            this.SumResult.Size = new System.Drawing.Size(100, 25);
+            this.SumResult.TabIndex = 4;
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SumResult);
+            this.Controls.Add(this.Sum2);
+            this.Controls.Add(this.Sum1);
+            this.Controls.Add(this.SumNumbers);
+            this.Controls.Add(this.HelloLabel);
+            this.Name = "Calculator";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label HelloLabel;
+        private System.Windows.Forms.Button SumNumbers;
+        private System.Windows.Forms.TextBox Sum1;
+        private System.Windows.Forms.TextBox Sum2;
+        private System.Windows.Forms.TextBox SumResult;
+    }
+}
+
---
+++ HelloC#Win/Calculator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloC_Win
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void HelloLabel_Click(object sender, EventArgs e)
+        {
+            HelloLabel.Text = "HelloC#";
+            int number1 = 10;
+            int number2 = 2;
+
+            int sum = number1 + number2;
+            HelloLabel.Text = sum.ToString();
+
+
+        }
+
+        private void SumNumbers_Click(object sender, EventArgs e)
+        {
+            int number1 = 0;
+            int number2 = 0;
+
+            if (String.IsNullOrWhiteSpace(Sum1.Text)){
+                MessageBox.Show("Sum1에 숫자를 입력해주세요.");
+                return;
+            }
+
+            if(int.TryParse(Sum1.Text, out number1) == false)
+            {
+                MessageBox.Show("Sum1에 문자가 들어왔습니다. 숫자를 입력해주세요.");
+                return;
+            }
+
+            if (String.IsNullOrWhiteSpace(Sum2.Text))
+            {
+                MessageBox.Show("Sum2에 숫자를 입력해주세요.");
+                return;
+            }
+
+            if (int.TryParse(Sum2.Text, out number1) == false)
+            {
+                MessageBox.Show("Sum2에 문자가 들어왔습니다. 숫자를 입력해주세요.");
+                return;
+            }
+
+            int sum = Add(number1, number2);
+            SumResult.Text = sum.ToString();
+
+        }
+        
+        public int Add(int number1, int number2) 
+        { 
+            int sum = number1 + number2;
+            return sum;
+        }
+
+        public float Add(float number1, float number2)
+        {
+            float sum = number1 + number2;
+            return sum;
+        }
+    }
+}
